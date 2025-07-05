a750ca3e9fb0415e027e4423baf09ba7fb606d57
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-07-05 09:54:10 +0530

    the admin routing bug is fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,16 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import {Toaster} from 'react-hot-toast' 
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+
 
 const App = () => {
 
-  const isAdminRoute = useLocation().pathname.startsWith('admin')
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
       <Toaster/>
@@ -24,6 +30,13 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
         <Route path='/favorite' element={<Favorite/>}/>
+        <Route path='/admin/*' element={<Layout/>}> 
+          <Route index element={<Dashboard/>}/>
+          <Route path="add-shows" element={<AddShows/>}/>
+          <Route path="list-shows" element={<ListShows/>}/>
+          <Route path="list-bookings" element={<ListBookings/>}/>
+
+        </Route>
 
       </Routes>
       {!isAdminRoute && <Footer/>}
---
+++ client/src/components/Admin/AdminNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+
+const AdminNavbar = () => {
+  return (
+    <div className='flex  items-center justify-between px-6 md:px-10 h-16 border-gray-300/30'>
+        <Link to='/'>
+            
+        </Link>
+    </div>
+  )
+}
+
+export default AdminNavbar
