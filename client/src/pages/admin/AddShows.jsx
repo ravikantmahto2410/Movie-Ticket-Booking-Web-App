0cc6cc67b01147796fcdaafa87fb4f6de5e0af89
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-08-04 16:59:23 +0530

    Add  date and time selection for movies

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets'
 import Loading from '../../components/Loading'
 import Title from '../../components/Admin/Title'
-import { CheckIcon, StarIcon } from 'lucide-react'
+import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react'
 import { kConverter } from '../../lib/kConverter'
 const AddShows = () => {
   const currency = import.meta.env.VITE_CURRENCY
@@ -15,6 +15,35 @@ const AddShows = () => {
   const fetchNowPlayingMovies = async () => {
     setNowPlayingMovies(dummyShowsData)
   }
+
+  const handleDateTimeAdd = () => {
+    if(!dateTimeInput) return;
+    const [date, time] = dateTimeInput.split("T")
+    if(!date || !time) return ;
+    
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || []
+      if(!times.includes(time)){
+        return {...prev,[date] : [...times, time] }
+      }
+      return prev;
+    })
+  }
+
+  const handleRemoveTime = (date, time) => {
+    setDateTimeSelection((prev) => {
+      const filteredTimes = prev[date].filter((t) => t !== time);
+      if(filteredTimes.length === 0){
+        const{[date] : _, ...rest} = prev
+        return rest;
+      }
+      return {
+        ...prev,
+        [date] : filteredTimes
+      }
+    })
+  }
+
 
   useEffect(() => {
     fetchNowPlayingMovies();
@@ -54,12 +83,80 @@ const AddShows = () => {
                   <CheckIcon className='w-4 h-4 text-white' strokeWidth={2.5}/>
                 </div>
               )}
+              <p className='font-medium truncate'>{movie.title}</p>
+              <p className='text-gray-400 text-sm'>{movie.release_date}</p>
 
             </div>
           ))}
 
         </div>
       </div>
+      {/* SHow price Input */}
+      <div className="mt-8">
+          <label className="block text-sm font-medium mb-2">Show Price</label>
+          <div className='inline-flex items-center gap-32 border border-gray-600
+          px-3 py-2 rounded-md'>
+            <p className='text-gray-400 text-sm'>{currency}</p>
+            <input
+              min={0}
+              type="number"
+              value={showPrice}
+              onChange={(e) => setShowPrice(e.target.value)}
+              placeholder="Enter show price"
+              className='outline-none'
+            />
+          </div>
+      </div>
+
+      {/*Date and Time Selection  */}
+      <div className='mt-6'>
+        <label className="block text-sm font-medium mb-2">Select Date and Time</label>
+        <div className='inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg'>
+          <input 
+            type="datetime-local"
+            value={dateTimeInput}
+            onChange={(e) => setDateTimeInput(e.target.value)}
+            className='outline-none rounded-md'
+          />
+          <button 
+          onClick={handleDateTimeAdd}
+          className='bg-primary/80 text-white px-3 py-2 text-sm rounded-lg
+          hover:bg-primary cursor-pointer'>
+            Add Time
+          </button>
+        </div>
+      </div>
+
+      {/* Diplay selected Time */}
+      {Object.keys(dateTimeSelection).length > 0 && (
+        <div className="mt-6">
+          <h2 className="mb-2">Selected Date-Time</h2>
+          <ul className="space-y-3">
+            {Object.entries(dateTimeSelection).map(([date,times]) => (
+              <li key={date}>
+                <div className='font-medium'>{date}</div>
+                <div className='flex flex-wrap gap-2 mt-1 text-sm'>
+                  {times.map((time) => (
+                    <div 
+                      key = {time}
+                      className='border border-primary px-2 py-1 flex items-center rounded'>
+                      <span>{time}</span>
+                      <DeleteIcon
+                        onClick={() => handleRemoveTime(date,time)} width={15}
+                        className="ml-2 text-red-500 hover:text-red-700 curosr-pointer"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90
+      transition-all cursor-pointer'>
+        Add Show
+      </button>
     </>
   ) : <Loading/>
 }
