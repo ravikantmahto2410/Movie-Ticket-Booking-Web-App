0091eb855fcacf118ba9369a7bfd256fec418525
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-08-04 18:40:05 +0530

    intigrate the clerk and inngest for authentication

---
+++ server/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        _id:{type: String, required:true},
+        name:{type: String, required:true},
+        email:{type: String, required:true},
+        image:{type: String, required:true},
+        
+        
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
+
