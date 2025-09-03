efc07e9c1bedccf63edce1face753f411bf45cb3
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-09-03 18:01:36 +0530

    admin can see the dashboard data successfully

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -5,7 +5,13 @@ import Loading from '../../components/Loading';
 import Title from '../../components/Admin/Title';
 import BlurCircle from '../../components/BlurCircle';
 import { dateFormat } from '../../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
+
+
 const Dashboard = () => {
+
+  const {axios, getToken, user, image_base_url} = useAppContext()
 
   const currency  = import.meta.env.VITE_CURRENCY
 
@@ -42,13 +48,31 @@ const Dashboard = () => {
   ]
 
   const fetchDashboardData = async () =>  {
-    setDashboardData(dummyDashboardData)
-    setLoading(false)
+    try {
+      const { data } = await axios.get("/api/admin/dashboard",{
+        headers:{
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+
+      if(data.success){
+        setDashboardData(data.dashboardData)
+        setLoading(false)
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error("Error fetching dashboard data:", error)
+    }
   }
 
   useEffect(() => {
-    fetchDashboardData();
-  },[])
+    if(user){
+      fetchDashboardData();
+    }
+    
+  },[user])
 
 
   return !loading ? (
@@ -79,7 +103,7 @@ const Dashboard = () => {
             key={show._id}
             className='w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border
             border-primary/20 hover:-translate-y-1 transition duration-300'>
-              <img src={show.movie.poster_path} alt='' className='h-60 w-full object-cover'/>
+              <img src={image_base_url + show.movie.poster_path} alt='' className='h-60 w-full object-cover'/>
               <p className='font-medium p-2 truncate'>{show.movie.title}</p>
               <div className='flex items-center justify-between px-2'>
                 <p className='text-lg font-medium'>{currency} {show.showPrice}</p>
