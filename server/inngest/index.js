d40b516be9547079e8080eea1d6d4bcdae7714a1
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-09-07 13:46:57 +0530

    bugs in tags in inngest function fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -101,11 +101,12 @@ const sendBookingConfirmationEmail = inngest.createFunction(
         await sendEmail({
             to: booking.user.email,
             subject: `Payment Confirmation : "${booking.show.movie.title}" booked!`,
-            body: `<div style="font-family: Arial, snas-serif; line-height:1.5;">
+            body: `<div style="font-family: Arial, sans-serif; line-height:1.5;">
                 <h2>Hi ${booking.user.name},</h2>
-                <p>Your booking for <strong style="color:#F84565;">
+                <p>Your booking for <strong style="color: #F84565;">
                     "${booking.show.movie.title}"
                 </strong>
+                is confirmed.
                 </p>
                 <p>
                     <strong>Date:</strong>${new Date(booking.show.showDateTime).toLocaleDateString('en-US', {timeZone:
@@ -113,7 +114,7 @@ const sendBookingConfirmationEmail = inngest.createFunction(
                     <strong>Time:</strong> ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US',{timeZone:
                     'Asia/Kolkata'})}
                 </p>
-                <p>Enjoy the show</p>
+                <p>Enjoy the show!</p>
                 <p>Thanks for booking with us!<br/>--Entertaino Team</p>
             </div>`
         })
