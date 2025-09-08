2e8c02cef6ae9de036e79c770cba004229a14305
ravikantmahto2410 <office.rakamahto2002@gmail.com>
2025-09-08 14:01:37 +0530

    imported timeformat in seatLayout

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -7,6 +7,7 @@ import isoTimeFormat from '../lib/isoTimeFormat';
 import BlurCircle from '../components/BlurCircle';
 import toast from 'react-hot-toast'
 import { useAppContext } from '../context/AppContext';
+import {isoTimeFormat } from '../lib/isoTimeFormat'
 const SeatLayout = () => {
 
   const groupRows = [["A", "B"], ["C", "D"],["E","F"],["G","H"],["I","J"]]
